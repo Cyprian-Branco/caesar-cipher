a612284a0d4c0b1f924e797257d469d4d039a7d0
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-11 14:27:37 +0300

    add test method on object

--- src/test/java/CipherDecryptTest.java
+++ src/test/java/CipherDecryptTest.java
@@ -1,5 +1,13 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CipherDecryptTest {
+    @Test
+    public void testCipherDecrypt_checkText_String(){
+        CipherDecrypt cipherdecrypt = new CipherDecrypt("st", 1);
+        assertEquals(true, cipherdecrypt instanceof CipherDecrypt);
+    }
+
 
 }
