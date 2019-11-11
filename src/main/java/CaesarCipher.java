b292234916a6843b372583338351e02eaa369e03
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-11 12:29:08 +0300

    delete files

---
+++ src/main/java/CaesarCipher.java
@@ -0,0 +1,11 @@
+public class CeasorCipher {
+    private String text;
+    private int key;
+    public static String Encrypt(String text, int key){
+        this.text = text;
+        this.key = key;
+    }
+
+
+
+}
