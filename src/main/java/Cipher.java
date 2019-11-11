1f9e5895701e20cafe36d0902db1c02e73d1c369
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-11 12:55:30 +0300

    check string and add encryption key

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -12,8 +12,13 @@ public class Cipher {
     public void Encrypt() {
         StringBuffer encryption = new StringBuffer();
         for (int i = 0; i < text.length(); i++) {
-
-
+            if (Character.isUpperCase(this.text.charAt(i))) {
+                char e = (char) (((int) this.text.charAt(i) + this.key - 65) % 26 + 65);
+                encryption.append(e);
+            } else {
+                char e = (char) (((int) this.text.charAt(1) + this.key - 97) % 26 + 97);
+                encryption.append(e);
+            }
         }
     }
 }
