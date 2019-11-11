13e7eeb4d853c7cd4c48592b564f9463e739b191
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-11 14:14:31 +0300

    initialization of variables

--- src/main/java/CipherDecrypt.java
+++ src/main/java/CipherDecrypt.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class CipherDecrypt {
+    private text;
+    private key;
+    public CipherDecrypt( String text, int key){
+        this.text = text;
+        this.key = key;
+    }
 }
