9aadc420a7b88e0b64955ec902afcc7297d77804
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-11 15:42:39 +0300

    add input option choices

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class App {
+    public static void main (String[] args){
+        Scanner option = new Scanner(System.in);
+        System.out.println("choice\n 1.Encrypt\n 2.Decrypt\n3.Exit");
+
+        String choice = option.nextLine();
+
+        if (choice.equals == 1){
+            
+        }
+    }
 }
