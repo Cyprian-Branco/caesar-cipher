e588e3d44c8dac9ce7c6dc48c57ecde41e5300ff
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-11 10:43:55 +0300

    add method for calculating new encryption

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,6 +1,7 @@
 public class Encrypt {
     private String text;
     private int shiftKey;
+    private StringBuffer Encryption;
 
     public Encrypt(String text, int shiftKey){
         this.text = text;
@@ -13,11 +14,16 @@ public class Encrypt {
     public int getShiftKey(){
         return shiftKey;
     }
-    public <Encryption> String Encryption(){
-        for (int i=0; i<text.length(); i++){
+        public <Encryption> void Encryption(){
+            for (int i=0; i<text.length(); i++){
             if(Character.isUpperCase(this.text.charAt(i))){
-                
+                char e = (char) (((int)this.text.charAt(i) + this.shiftKey -65) % 26 + 65);
+                Encryption.append(e);
+            }else{
+                char e = (char) (((int)this.text.charAt(1) + this.shiftKey -97) % 26 + 97);
+                Encryption.append(e);
             }
+
 
         }
 
