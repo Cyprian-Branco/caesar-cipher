b6dcc3316d3666ef9833434aa360c8f774adabd4
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-11 21:14:05 +0300

    take in input string

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,12 +2,19 @@ import java.util.Scanner;
 
 public class App {
     public static void main (String[] args){
+        Cipher cipher = new Cipher();
         Scanner option = new Scanner(System.in);
         System.out.println("choice\n 1.Encrypt\n 2.Decrypt\n3.Exit");
 
         Integer choice = option.nextInt();
 
+
         if (choice == 1){
+            Scanner inputText = new Scanner(System.in);
+            System.out.println("Enter a Text: ");
+            String text = inputText.nextLine();
+            System.out.println("The input text is: " + text );
+            
 
         }
     }
