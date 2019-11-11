58174fabbfbdf1c3b54c0e0f79366997d66c35e2
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-11 12:41:26 +0300

    add method for testing

--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -1,5 +1,13 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CipherTest {
+    @Test
+    public void testCipher_checkText_String(){
+        Cipher cipher = new Cipher("st", 1);
+        assertEquals("rs", cipher.getText());
+    }
+
 
 }
