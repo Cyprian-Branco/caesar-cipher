5a2e5f7128abc69fb64eebf9c4cd8eeb8bd0ecea
Cyprian Bowen <branco1lelgo@gmail.com>
2019-11-11 12:40:36 +0300

    initialize expected variables

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,3 +1,11 @@
 public class Cipher {
-    
+    private String text;
+    private int key;
+
+    public Cipher(String text, int key){
+        this.text = text;
+        this.key = key;
+    }
+
+
 }
